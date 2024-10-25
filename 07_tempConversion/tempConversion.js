9a1e34d30761836a7eb2321ca8a4a61d053bcf38
Klaska <laska.rayl@gmail.com>
2024-10-25 22:22:58 +0200

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahren) {
+  let conFahren = (fahren - 32) * 5/9;
+  let roundedConFahren = Number(conFahren.toFixed(1));
+  return roundedConFahren; 
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let conCelsius = celcius * 9/5 + 32;
+  let roundedConCelsius = Number(conCelsius.toFixed(1));
+  return roundedConCelsius;
 };
 
 // Do not edit below this line
